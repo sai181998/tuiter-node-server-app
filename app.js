5af037ea3233a6ad34d4f9de6cf7bc7bac9aef52
lurker <lurker@chromium.org>
2023-04-08 17:17:11 -0400

    a9 completed

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController  from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors'
+import mongoose from "mongoose";
+
+//mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 app.use(express.json());
@@ -12,7 +17,7 @@ HelloController(app)
 UserController(app)
 TuitsController(app);
 
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 4001);
 
 
 //app.listen(4000)
