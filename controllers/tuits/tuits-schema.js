466b36bb7cfebdf9adfe6c1e9df63c54bcd45dd8
lurker <lurker@chromium.org>
2023-04-09 00:40:02 -0400

    a9 updated

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,8 +1,18 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
-  tuit: String,
-  likes: Number,
-  liked: Boolean,
+  topic: String,
+    userName: String,
+    time: String,
+    title: String,
+    image: String,
+    liked: Boolean,
+    replies: Number,
+    retuits: Number,
+    likes: Number,
+    dislikes: Number,
+    disliked: Boolean,
+    handle: String,
+    tuit: String,
 }, {collection: 'tuits'});
 
 export default schema;
